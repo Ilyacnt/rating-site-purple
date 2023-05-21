e0dcfa672c5c7e0bf21a05387e4f1d55eee632b4
Ilya Baranchikov <ilyabaranchikov@MacBook-Air-Ilya.local>
2023-05-21 15:58:37 +0300

    Button component added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import { Heading } from '@/components'
+
 export default function Home(): JSX.Element {
-    return <div>HOME</div>
+    return (
+        <div>
+            <Heading tag="h1">Some shit</Heading>
+            <Heading tag="h2">Some h2 shit</Heading>
+            <Heading tag="h3">Some h3 shit</Heading>
+        </div>
+    )
 }
