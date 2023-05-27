3ac3fff0a581ab579f6858dc6e1a676395c34785
Ilya Baranchikov <ilyabaranchikov@MacBook-Air-Ilya.local>
2023-05-27 15:04:23 +0300

    Grid layout added

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -2,17 +2,16 @@ import { Footer } from './Footer/Footer'
 import { Header } from './Header/Header'
 import { LayoutProps } from './Layout.props'
 import { Sidebar } from './Sidebar/Sidebar'
+import styles from './Layout.module.css'
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
     return (
-        <>
-            <Header />
-            <div>
-                <Sidebar />
-                <div>{children}</div>
-            </div>
-            <Footer />
-        </>
+        <div className={styles.wrapper}>
+            <Header className={styles.header} />
+            <Sidebar className={styles.sidebar} />
+            <div className={styles.main}>{children}</div>
+            <Footer className={styles.footer} />
+        </div>
     )
 }
 
